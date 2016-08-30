fd61b706f10180deade76dfd4432a229b928b073
Eduardo Lanchares <eduardo@elanchares.com>
2016-08-31 00:44:19 +0200

    Use electron for running tests :muscle:

--- TODO.md
+++ TODO.md
@@ -1,9 +1,12 @@
 ## TODO
 * Use npm start to run electron app. Implement page not found on development
 and show a warn with detailed message on how to start webpack server.
+* Setup ESLint
 
 ## IN PROGRESS
-* Try out director for routes
+* Add bootstrap and some page layout
 
 ## DONE
 * Try out dexie package
+* Try out director for routes
+* Use electron to run tests (http://webuild.envato.com/blog/running-headless-javascript-testing-with-electron-on-any-ci-server/)
--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -1,21 +1,3 @@
-var conf = require('../../config/babel.dev');
+var conf = require('../../config/babel.test');
 
 require('babel-core/register')(conf);
-
-// When using full dom rendering with enzyme, we need to setup jsdom first in
-// order to have a dom engine working.
-
-var jsdom = require('jsdom').jsdom;
-var exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
-global.navigator = {
-  userAgent: 'node.js'
-};
