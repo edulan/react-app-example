3edc70e0e89b3adf8fe881d9d07599584112aee9
Eduardo Lanchares <eduardo@elanchares.com>
2016-08-30 23:21:25 +0200

    Use a subset of babel es2015 plugins (targeting electron)

--- config/babel.dev.js
+++ config/babel.dev.js
@@ -1,16 +1,31 @@
 module.exports = {
   babelrc: false,
   presets: [
-    'babel-preset-es2015',
-    'babel-preset-es2016',
-    'babel-preset-react'
+    'babel-preset-react',
+    'babel-preset-stage-1'
   ].map(require.resolve),
   plugins: [
-    'babel-plugin-syntax-decorators',
-    'babel-plugin-syntax-trailing-function-commas',
-    'babel-plugin-transform-class-properties',
+    'babel-plugin-transform-decorators-legacy',
+    // es2015 preset plugins
+    'babel-plugin-transform-es2015-block-scoped-functions',
+    'babel-plugin-transform-es2015-block-scoping',
+    'babel-plugin-transform-es2015-computed-properties',
+    'babel-plugin-transform-es2015-destructuring',
+    'babel-plugin-transform-es2015-duplicate-keys',
+    'babel-plugin-transform-es2015-for-of',
+    'babel-plugin-transform-es2015-function-name',
+    'babel-plugin-transform-es2015-literals',
+    'babel-plugin-transform-es2015-modules-commonjs',
+    'babel-plugin-transform-es2015-object-super',
+    'babel-plugin-transform-es2015-shorthand-properties',
+    'babel-plugin-transform-es2015-spread',
+    'babel-plugin-transform-es2015-sticky-regex',
+    'babel-plugin-transform-es2015-template-literals',
+    'babel-plugin-transform-es2015-typeof-symbol',
+    'babel-plugin-transform-es2015-unicode-regex',
+    'babel-plugin-transform-regenerator',
     'babel-plugin-transform-object-rest-spread',
-    'babel-plugin-transform-decorators-legacy'
+    'babel-plugin-syntax-trailing-function-commas'
   ].map(require.resolve).concat([
     [require.resolve('babel-plugin-transform-runtime'), {
       helpers: false,
