fac11f64db9977ced44ae0013895fa30aba6df5d
Eduardo Lanchares <eduardo@elanchares.com>
2016-09-06 09:03:44 +0200

    Fix wierd setState warning

--- src/components/Login/Form.js
+++ src/components/Login/Form.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
+// NOTE: Somehow extending Component is causing an error when updating state.
+// With createClass is not happening anymore.
+const Form = React.createClass({
+  getInitialState() {
+    return {
       email: 'foo@example.org',
       password: ''
     };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  },
 
   onChange(event) {
     if (!event.target.name) return;
 
+    event.preventDefault();
     this.setState({[event.target.name]: event.target.value});
-  }
+  },
 
   onSubmit(event) {
     const { email, password } = this.state;
 
     event.preventDefault();
     this.props.onSubmit({email, password});
-  }
+  },
 
   render() {
     return (
@@ -62,6 +60,6 @@ class Form extends Component {
       </form>
     );
   }
-}
+});
 
 export default Form;
