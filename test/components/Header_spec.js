50afba7b6c9b00e1857ceb1c4fa8d8d625faaa60
Eduardo Lanchares <eduardo@elanchares.com>
2016-09-23 20:02:29 +0200

    Fix Header specs

--- test/components/Header_spec.js
+++ test/components/Header_spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { observable } from 'mobx';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
 
@@ -6,17 +7,23 @@ import Header from 'components/Header';
 
 describe('<Header/>', () => {
   it('shows login link if current section is login', () => {
-    const wrapper = mount(<Header section='login'/>);
+    const store = observable({currentView: {name: 'login'}});
+
+    const wrapper = mount(<Header view={store}/>);
     expect(wrapper.find('a')).to.have.attr('href').equal('#/login');
   });
 
-  it('shows home link if current section is home', () => {
-    const wrapper = mount(<Header section='users'/>);
+  it('shows home link if current section is users', () => {
+    const store = observable({currentView: {name: 'users'}});
+
+    const wrapper = mount(<Header view={store}/>);
     expect(wrapper.find('a')).to.have.attr('href').equal('#/home');
   });
 
   it('shows no link if current section is unknown', () => {
-    const wrapper = mount(<Header section='foo'/>);
+    const store = observable({currentView: {name: 'unknown'}});
+
+    const wrapper = mount(<Header view={store}/>);
     expect(wrapper.find('a')).to.have.length(0);
   });
 });
